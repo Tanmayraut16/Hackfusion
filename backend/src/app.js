f1ef2952c8fcd069a5673b9d74b3c80979ea6da3
Akash Masurkar <akashmasurkar009@gmail.com>
2025-02-16 16:18:50 +0530

    solved error and comment match

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,22 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import studentRouter from "../src/routes/student.routes.js"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import studentRouter from "../src/routes/student.routes.js";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use('/api/student-login', studentRouter);
+app.use("/api/student-login", studentRouter);
 
-export { app }
+export { app };
